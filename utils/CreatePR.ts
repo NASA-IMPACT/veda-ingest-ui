d3a28817ccbca39d6a550a6d4b7f3a5b7cfddb8b
Stephen Kilbourn <skilbourn@element84.com>
2024-12-06 14:12:25 -0700

    restore octokit bring out env variables from amplify build

--- utils/CreatePR.ts
+++ utils/CreatePR.ts
@@ -14,13 +14,10 @@ const CreatePR = async (data: unknown ) => {
 
     //@ts-expect-error testing
     const collectionName = data['collection'];
-    console.log('data in CreatePR')
-    console.log(data)
     
     console.log('collectionName', collectionName)
     // prettify stringify to preserve json formatting
     const content = JSON.stringify(data, null, 2);
-      console.log('content', content)
       const targetPath = 'ingestion-data/staging/dataset-config';
       const fileName = collectionName;
       const path = `${targetPath}/${fileName}.json`;
@@ -43,78 +40,76 @@ const CreatePR = async (data: unknown ) => {
       installationId,
     });
 
-    // const octokit = new Octokit({
-    //   auth: token,
-    // });
+    const octokit = new Octokit({
+      auth: token,
+    });
 
-    console.log(token)
     // // Get the current target branch reference to get the sha
-    // const sha = await octokit.rest.git.getRef({
-    //   owner,
-    //   repo,
-    //   ref: `heads/${targetBranch}`,
-    // });
+    const sha = await octokit.rest.git.getRef({
+      owner,
+      repo,
+      ref: `heads/${targetBranch}`,
+    });
 
 
     // // Get the tree associated with master, and the content
     // // of the template file to open the PR with.
-    // const tree = await octokit.rest.git.getTree({
-    //   owner,
-    //   repo,
-    //   tree_sha: sha.data.object.sha,
-    // });
+    const tree = await octokit.rest.git.getTree({
+      owner,
+      repo,
+      tree_sha: sha.data.object.sha,
+    });
 
     // // Create a new blob with the content in formData
-    // const blob = await octokit.rest.git.createBlob({
-    //   owner,
-    //   repo,
-    //   content,
-    //   encoding: 'utf-8',
-    // });
+    const blob = await octokit.rest.git.createBlob({
+      owner,
+      repo,
+      content,
+      encoding: 'utf-8',
+    });
 
-    // const newTree = await octokit.rest.git.createTree({
-    //   owner,
-    //   repo,
-    //   tree: [
-    //     {
-    //       path,
-    //       sha: blob.data.sha,
-    //       mode: '100644',
-    //       type: 'blob',
-    //     },
-    //   ],
-    //   base_tree: tree.data.sha,
-    // });
+    const newTree = await octokit.rest.git.createTree({
+      owner,
+      repo,
+      tree: [
+        {
+          path,
+          sha: blob.data.sha,
+          mode: '100644',
+          type: 'blob',
+        },
+      ],
+      base_tree: tree.data.sha,
+    });
 
     // // Create a commit and a reference using the new tree
-    // const newCommit = await octokit.rest.git.createCommit({
-    //   owner,
-    //   repo,
-    //   message: `Create ${path}`,
-    //   tree: newTree.data.sha,
-    //   parents: [sha.data.object.sha],
-    // });
+    const newCommit = await octokit.rest.git.createCommit({
+      owner,
+      repo,
+      message: `Create ${path}`,
+      tree: newTree.data.sha,
+      parents: [sha.data.object.sha],
+    });
 
-    // await octokit.rest.git.createRef({
-    //   owner,
-    //   repo,
-    //   ref: `refs/heads/${branchName}`,
-    //   sha: newCommit.data.sha,
-    // });
+    await octokit.rest.git.createRef({
+      owner,
+      repo,
+      ref: `refs/heads/${branchName}`,
+      sha: newCommit.data.sha,
+    });
 
     // // open PR with new file added to targetBranch
-    // const pr = await octokit.rest.pulls.create({
-    //   owner,
-    //   repo,
-    //   head: branchName,
-    //   base: targetBranch,
-    //   title: `Create ${path}`,
-    // });
+    const pr = await octokit.rest.pulls.create({
+      owner,
+      repo,
+      head: branchName,
+      base: targetBranch,
+      title: `Create ${path}`,
+    });
 
-    // const pr_url = pr.data.html_url;
-    // console.log(`PR opened at URL ${pr_url}`);
-    // return pr_url;
-    return('http://www.test.com')
+    const pr_url = pr.data.html_url;
+    console.log(`PR opened at URL ${pr_url}`);
+    return pr_url;
   } catch (error) {
     if (error instanceof RequestError) {
       // branch with branchName already exists
