4984c3dce9f46d3cbc3baad6c133fc0d657246d0
Stephen Kilbourn <skilbourn@element84.com>
2024-12-09 14:53:15 -0700

    try with basic form to debug

--- app/create-ingest/page.tsx
+++ app/create-ingest/page.tsx
@@ -1,26 +1,29 @@
 
 "use client"
-import { useState } from 'react';
+// import { useState } from 'react';
 import AppLayout from '../../components/Layout';
 
-import { Spin } from 'antd';
+import DebugForm from '@/components/DebugForm';
+// import { Spin } from 'antd';
 
-import ValidationForm from '../../components/ValidationForm';
-import ErrorModal from '../../components/ErrorModal';
-import SuccessModal from '../../components/SuccessModal';
+// import ValidationForm from '../../components/ValidationForm';
+// import ErrorModal from '../../components/ErrorModal';
+// import SuccessModal from '../../components/SuccessModal';
 
 const CreateIngest = function CreateIngest() {
-  const [status, setStatus] = useState('idle');
-  const [collectionName, setCollectionName] = useState('');
-  const [apiErrorMessage, setApiErrorMessage] = useState('');
-  const [pullRequestUrl, setPullRequestUrl] = useState('');
+  // const [status, setStatus] = useState('idle');
+  // const [collectionName, setCollectionName] = useState('');
+  // const [apiErrorMessage, setApiErrorMessage] = useState('');
+  // const [pullRequestUrl, setPullRequestUrl] = useState('');
 
   return (
     <AppLayout>
-          <ValidationForm setStatus={setStatus} setCollectionName={setCollectionName} setApiErrorMessage={setApiErrorMessage} setPullRequestUrl={setPullRequestUrl} />
-          {status === 'loading' && <Spin fullscreen />}
+        <DebugForm />
+          {/* <ValidationForm setStatus={setStatus} setCollectionName={setCollectionName} setApiErrorMessage={setApiErrorMessage} setPullRequestUrl={setPullRequestUrl} /> */}
+          
+          {/* {status === 'loading' && <Spin fullscreen />}
           {status === 'error' && <ErrorModal collectionName={collectionName} apiErrorMessage={apiErrorMessage} />}
-          {status === 'success' && <SuccessModal setStatus={setStatus} collectionName={collectionName} pullRequestUrl={pullRequestUrl} />}
+          {status === 'success' && <SuccessModal setStatus={setStatus} collectionName={collectionName} pullRequestUrl={pullRequestUrl} />} */}
     </AppLayout>
   );
 };
