9f026094759758ae95c070501662ee68a044fd2a
Stephen Kilbourn <skilbourn@element84.com>
2024-12-10 16:33:02 -0700

    design cleanup

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 
 import { signOut } from 'aws-amplify/auth';
 import { LogoutOutlined } from '@ant-design/icons'
@@ -10,18 +10,21 @@ async function handleSignOut() {
 
 const LogoutButton = ({collapsed}: {collapsed: boolean}) => {
   return (
-    <Button
-    type='primary'
-    danger
-    onClick={handleSignOut}
-    icon={<LogoutOutlined/>}
-    block
-    style={{
-      width: 'calc(100% - 8px)',
-      marginInline: '4px',
-    }}
-    >{collapsed ? '' : 'Sign out'}</Button>
-
+    <Tooltip placement="right" title={collapsed ? 'Sign out' : ''} >
+      <Button
+      type='primary'
+      danger
+      onClick={handleSignOut}
+      icon={<LogoutOutlined/>}
+      block
+      style={{
+        width: 'calc(100% - 8px)',
+        marginInline: '4px',
+        marginTop: '16px',
+      }}>
+        {collapsed ? '' : 'Sign out'}
+        </Button>
+      </Tooltip>
   )
 }
 
