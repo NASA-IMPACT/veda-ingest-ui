8ed267ab57a9dc40bc68f5e62db50e7d61c75055
Stephen Kilbourn <skilbourn@element84.com>
2024-12-11 16:55:45 -0700

    add auth middleware

---
+++ components/AmplifyProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Amplify } from "aws-amplify";
+import {config} from "@/utils/aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+
+// @ts-expect-error something
+Amplify.configure({ ...config, ssr: true });
+export default function AmplifyProvider({ children }: React.PropsWithChildren) {
+  return <>{children}</>;
+}
---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { runWithAmplifyServerContext } from "@/utils/amplify-server-util";
+import { AmplifyServer } from "aws-amplify/adapter-core";
+
+// The fetchAuthSession is pulled as the server version from aws-amplify/auth/server
+import { fetchAuthSession } from "aws-amplify/auth/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // The runWithAmplifyServerContext will run the operation below
+  // in an isolated matter.
+  const authenticated = await runWithAmplifyServerContext({
+    nextServerContext: { request, response },
+    operation: async (contextSpec: AmplifyServer.ContextSpec) => {
+      try {
+        
+        // The fetch will grab the session cookies
+        const session = await fetchAuthSession(contextSpec, {});
+        return session.tokens !== undefined;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  });
+
+  // If user is authenticated then the route request will continue on
+  if (authenticated) {
+    return response;
+  }
+
+  // user is not authenticated
+  return NextResponse.json({ message: 'Not Authenticated'}, {status: 401});
+}
+
+// This config will match all routes accept /login, /api, _next/static, /_next/image
+// favicon.ico
+export const config = {
+  matcher: ['/api/create-ingest' ],
+};
