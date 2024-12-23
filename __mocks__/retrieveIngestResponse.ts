6c44a3df8faf19b17f9cf29937c08251a9d1c75f
Stephen Kilbourn <skilbourn@element84.com>
2024-12-23 16:17:46 -0700

    Add unit tests (#15)

---
+++ __mocks__/retrieveIngestResponse.ts
@@ -0,0 +1,32 @@
+export const retrieveIngestResponse = {
+  "fileSha": "7ddc707171a723b929f866a738d6bbe2ab5f3256",
+  "filePath": "ingestion-data/staging/dataset-config/seededingest1.json",
+  "content": {
+      "links": [],
+      "spatial_extent": {
+          "xmin": 1,
+          "ymin": 1,
+          "xmax": 1,
+          "ymax": 1
+      },
+      "temporal_extent": {
+          "startdate": "2024-12-01",
+          "enddate": "2024-12-31"
+      },
+      "discovery_items": [
+          {
+              "prefix": "test",
+              "bucket": "test"
+          }
+      ],
+      "sample_files": [
+          "test"
+      ],
+      "data_type": "cog",
+      "stac_extensions": [],
+      "collection": "seeded ingest #1",
+      "title": "test",
+      "license": "test",
+      "description": "test"
+  }
+};
