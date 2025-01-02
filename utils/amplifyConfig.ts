3e9c2aace191e1da7c7349a6e7f3591e2066d7ad
Stephen Kilbourn <skilbourn@element84.com>
2025-01-02 16:45:48 -0700

    remove unused amplify Config

--- utils/amplifyConfig.ts
+++
@@ -1,6 +0,0 @@
-import { Amplify } from 'aws-amplify';
-import { config } from '@/utils/aws-exports';
-
-export const configureAmplify = () => {
-  Amplify.configure({ ...config }, { ssr: true });
-}
