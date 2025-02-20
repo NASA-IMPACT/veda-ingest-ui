e6f1f7dadec16395cc8d23931930e2a44d935e2f
Stephen Kilbourn <skilbourn@element84.com>
2025-02-20 11:21:19 -0700

    add screenshots and test step descriptions for Create Ingest Page

--- __tests__/playwright/CreateIngestPage.test.tsx
+++ __tests__/playwright/CreateIngestPage.test.tsx
@@ -67,52 +67,75 @@ const requiredConfig = {
 };
 
 test.describe('CreateIngest Page', () => {
-  test('create Ingest handles errors with pasted JSON', async ({ page }) => {
-    // Navigate to the page with COGControlsForm
-    await page.goto('/create-ingest');
+  test('create Ingest handles errors with pasted JSON', async ({
+    page,
+  }, testInfo) => {
+    await test.step('Navigate to the Create Ingest page', async () => {
+      await page.goto('/create-ingest');
+    });
 
-    await page.getByRole('tab', { name: /manual json edit/i }).click();
+    await test.step('switch to manual json edit tab', async () => {
+      await page.getByRole('tab', { name: /manual json edit/i }).click();
+    });
     await expect(
-      page.getByRole('button', { name: /apply changes/i })
+      page.getByRole('button', { name: /apply changes/i }),
+      'Apply Changes should be disabled if no changes are made'
     ).toBeDisabled();
 
-    await page.getByTestId('json-editor').fill('s3://test.com');
-    await page.getByRole('button', { name: /apply changes/i }).click();
-    await expect(page.getByText('Invalid JSON format.')).toBeVisible();
-    await page.getByTestId('json-editor').fill('{"validJSON": true}');
-    await page.getByRole('button', { name: /apply changes/i }).click();
-    await expect(page.getByText('Schema Validation Errors:')).toBeVisible();
-    const requiredProperties = [
-      'collection',
-      'title',
-      'description',
-      'license',
-      'discovery_items',
-      'spatial_extent',
-      'temporal_extent',
-      'sample_files',
-      'data_type',
-      'providers',
-      'item_assets',
-    ];
+    await test.step('paste a non-JSON string in the editor and check for error message', async () => {
+      await page.getByTestId('json-editor').fill('s3://test.com');
+      await page.getByRole('button', { name: /apply changes/i }).click();
+      await expect(page.getByText('Invalid JSON format.')).toBeVisible();
+    });
 
-    for (const property of requiredProperties) {
+    await test.step('paste a JSON not matching required schema in the editor and check for error message', async () => {
+      await page.getByTestId('json-editor').fill('{"validJSON": true}');
+      await page.getByRole('button', { name: /apply changes/i }).click();
+      await expect(page.getByText('Schema Validation Errors:')).toBeVisible();
+      const requiredProperties = [
+        'collection',
+        'title',
+        'description',
+        'license',
+        'discovery_items',
+        'spatial_extent',
+        'temporal_extent',
+        'sample_files',
+        'data_type',
+        'providers',
+        'item_assets',
+      ];
+
+      for (const property of requiredProperties) {
+        await expect(
+          page
+            .getByRole('listitem')
+            .filter({ hasText: `must have required property '${property}'` }),
+          `${property} error message should be visible`
+        ).toBeVisible();
+      }
+
       await expect(
         page
           .getByRole('listitem')
-          .filter({ hasText: `must have required property '${property}'` })
+          .filter({ hasText: 'must NOT have additional properties' }),
+        'additional property in JSON should create error'
       ).toBeVisible();
-    }
+    });
 
-    // additional property of "validJSON": true is present
-    await expect(
-      page
-        .getByRole('listitem')
-        .filter({ hasText: 'must NOT have additional properties' })
-    ).toBeVisible();
+    const errorMessagesScreenshot = await page.screenshot();
+    testInfo.attach(
+      'Default state of COG Viewer Form Controls for single band COG',
+      {
+        body: errorMessagesScreenshot,
+        contentType: 'image/png',
+      }
+    );
   });
 
-  test('create Ingest request submitted with pasted JSON', async ({ page }) => {
+  test('Create Ingest request submitted with pasted JSON', async ({
+    page,
+  }, testInfo) => {
     // Intercept and block the request
     await page.route('**/create-ingest', async (route, request) => {
       if (request.method() === 'POST') {
@@ -139,24 +162,49 @@ test.describe('CreateIngest Page', () => {
         await route.continue();
       }
     });
-    await page.goto('/create-ingest');
+    await test.step('Navigate to the Create Ingest page', async () => {
+      await page.goto('/create-ingest');
+    });
 
-    await page.getByRole('tab', { name: /manual json edit/i }).click();
+    await test.step('switch to manual json edit tab', async () => {
+      await page.getByRole('tab', { name: /manual json edit/i }).click();
+    });
+
     await expect(
-      page.getByRole('button', { name: /apply changes/i })
+      page.getByRole('button', { name: /apply changes/i }),
+      'Apply Changes should be disabled if no changes are made'
     ).toBeDisabled();
 
-    await page.getByTestId('json-editor').fill(JSON.stringify(requiredConfig));
-    await page.getByRole('button', { name: /apply changes/i }).click();
+    await test.step('paste a JSON with config options matching schema minimum', async () => {
+      await page
+        .getByTestId('json-editor')
+        .fill(JSON.stringify(requiredConfig));
+      const JSONScreenshot = await page.screenshot();
+      testInfo.attach('pasted JSON in editor', {
+        body: JSONScreenshot,
+        contentType: 'image/png',
+      });
+      await page.getByRole('button', { name: /apply changes/i }).click();
+    });
 
-    await validateFormFields(page, requiredConfig);
+    await test.step('validate that form tab displays with pasted json values now populated in form', async () => {
+      await validateFormFields(page, requiredConfig);
+    });
 
-    await page.getByRole('button', { name: /submit/i }).click();
+    const completedFormScreenshot = await page.screenshot({ fullPage: true });
+    testInfo.attach('frm with values pasted in JSON editor', {
+      body: completedFormScreenshot,
+      contentType: 'image/png',
+    });
+
+    await test.step('submit form and validate that POST body values match pasted config values', async () => {
+      await page.getByRole('button', { name: /submit/i }).click();
+    });
   });
 
-  test('create Ingest submits extra fields with toggle enabled', async ({
+  test('Create Ingest allows extra fields with toggle enabled', async ({
     page,
-  }) => {
+  }, testInfo) => {
     // Intercept and block the request
     await page.route('**/create-ingest', async (route, request) => {
       if (request.method() === 'POST') {
@@ -171,19 +219,34 @@ test.describe('CreateIngest Page', () => {
         await route.continue();
       }
     });
-    // Navigate to the page with COGControlsForm
-    await page.goto('/create-ingest');
+    await test.step('Navigate to the Create Ingest page', async () => {
+      await page.goto('/create-ingest');
+    });
 
-    await page.getByRole('tab', { name: /manual json edit/i }).click();
+    await test.step('switch to manual json edit tab', async () => {
+      await page.getByRole('tab', { name: /manual json edit/i }).click();
+    });
 
-    await page
-      .getByRole('checkbox', { name: /enforce strict schema/i })
-      .uncheck();
-    await page
-      .getByTestId('json-editor')
-      .fill(JSON.stringify({ ...requiredConfig, extraField: true }));
-    await page.getByRole('button', { name: /apply changes/i }).click();
+    await test.step('uncheck the Enforce Strict Schema  checkbox', async () => {
+      await page
+        .getByRole('checkbox', { name: /enforce strict schema/i })
+        .uncheck();
+    });
 
-    await page.getByRole('button', { name: /submit/i }).click();
+    await test.step('paste in a valid config with an additional field', async () => {
+      await page
+        .getByTestId('json-editor')
+        .fill(JSON.stringify({ ...requiredConfig, extraField: true }));
+      const JSONScreenshot = await page.screenshot();
+      testInfo.attach('pasted JSON in editor', {
+        body: JSONScreenshot,
+        contentType: 'image/png',
+      });
+      await page.getByRole('button', { name: /apply changes/i }).click();
+    });
+
+    await test.step('submit form and validate that POST body values match pasted config values including extra field', async () => {
+      await page.getByRole('button', { name: /submit/i }).click();
+    });
   });
 });
