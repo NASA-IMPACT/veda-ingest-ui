1aa0eb42df6db4d7c0937250bb06d010c7ed5e1a
Stephen Kilbourn <skilbourn@element84.com>
2025-05-16 17:13:12 -0600

    fix page flash make base page ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,32 @@
-'use client';
-
 import '@ant-design/v5-patch-for-react-19';
-import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { Spin } from 'antd';
-
 import AppLayout from '@/components/Layout';
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth';
 
 const DISABLE_AUTH = process.env.NEXT_PUBLIC_DISABLE_AUTH === 'true';
 
-const Home = function Home() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  const [isInitialLoading, setIsInitialLoading] = useState(true);
-
-  useEffect(() => {
-    if (status !== 'loading') {
-      setIsInitialLoading(false);
-    }
-    if (!DISABLE_AUTH && status === 'unauthenticated') {
-      router.push('/login');
-    }
-  }, [status, router]);
-
-  if (isInitialLoading) {
+export default async function Home() {
+  if (DISABLE_AUTH) {
     return (
       <AppLayout>
-        <div
-          style={{ display: 'flex', justifyContent: 'center', padding: 100 }}
+        <section
+          style={{
+            textAlign: 'center',
+            marginTop: 48,
+            marginBottom: 40,
+            padding: 100,
+          }}
         >
-          <Spin size="large" />
-        </div>
+          This application allows users to initiate the data ingest process.
+        </section>
       </AppLayout>
     );
+  }
+
+  const session = await auth();
+
+  if (!session) {
+    redirect('/login');
   }
 
   return (
@@ -50,6 +43,4 @@ const Home = function Home() {
       </section>
     </AppLayout>
   );
-};
-
-export default Home;
+}
