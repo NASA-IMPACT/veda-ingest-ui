a3fdbe00ec009395e85a5f2f17323349251d283a
Stephen Kilbourn <skilbourn@element84.com>
2025-08-06 12:25:48 -0600

    fix cropping & formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,16 @@ import { Inter } from 'next/font/google';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import 'leaflet/dist/leaflet.css';
 import { SessionProvider } from 'next-auth/react';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'VEDA Ingest UI',
+  description: 'A user interface for VEDA data ingestion.',
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 export default function RootLayout({
