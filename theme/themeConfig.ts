63a5826983f473f7e8d0ccee01f1f12d622d64ed
Stephen Kilbourn <skilbourn@element84.com>
2025-08-07 19:47:21 -0600

    add red and blue color tokens for ant-d

---
+++ theme/themeConfig.ts
@@ -0,0 +1,13 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    // Override the primary color with NASA blue
+    colorPrimary: '#0B3D91',
+
+    // Override the error color with NASA red
+    colorError: '#FC3D21',
+  },
+};
+
+export default theme;
