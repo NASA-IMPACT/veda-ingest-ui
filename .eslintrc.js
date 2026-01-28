3baf45201c8a56193a5beae965695196b1938108
Stephen Kilbourn <skilbourn@element84.com>
2026-01-28 13:23:43 -0700

    chore: update ESLint config and dependencies for Next.js 16

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   extends: ['next', 'prettier'],
-  plugins: ['react', '@typescript-eslint'],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
   overrides: [
     {
       files: ['__tests__/playwright/**'],
