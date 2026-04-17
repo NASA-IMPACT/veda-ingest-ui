24a4ea6ce3280cbaf0a7c09f2998135bc09f185c
Stephen Kilbourn <skilbourn@element84.com>
2026-04-16 18:35:09 -0600

    fix type error

--- utils/CleanAndPrettifyJson.ts
+++ utils/CleanAndPrettifyJson.ts
@@ -7,10 +7,11 @@ export const CleanAndPrettifyJSON = (data: FormLikeData): string => {
   const cleanedData: FormLikeData = { ...data };
 
   if (typeof cleanedData.renders === 'object' && cleanedData.renders !== null) {
-    Object.entries(cleanedData.renders).forEach(([renderKey, renderValue]) => {
+    const renders = cleanedData.renders as Record<string, unknown>;
+    Object.entries(renders).forEach(([renderKey, renderValue]) => {
       if (typeof renderValue === 'string' && renderValue.trim() !== '') {
         try {
-          cleanedData.renders![renderKey] = JSON.parse(renderValue);
+          renders[renderKey] = JSON.parse(renderValue);
         } catch {
           console.warn(
             `Invalid JSON in 'renders ${renderKey}' field. Keeping as string.`
